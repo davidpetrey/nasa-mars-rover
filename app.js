11f9fdaaef82147da0bec2ada19266a9a50e5b2e
David Petrey <davidpetrey@users.noreply.github.com>
2022-05-02 23:08:31 -0500

    now its ready

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', () => {
 /* https://api.nasa.gov/ */
 
 	let maxSol;
 	let manifest;
 	let solsWithPics;
-
 
 	let getManifest = (rover) => {
 		$.ajax({
@@ -15,17 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
 		.done(function(data, textStatus, jqXHR) {
 			console.log(jqXHR.status, textStatus);
 			console.log(data);
-			console.log(data.photo_manifest.max_sol);
 			// console.log(jqXHR);
 			manifest = data.photo_manifest;
 			maxSol = data.photo_manifest.max_sol;
-			console.log(manifest);
-
 
 			solsWithPics = manifest.photos.map(arr => arr.sol);
-
-			console.log(solsWithPics);
-
 
 			solDescript.innerText = "Please enter a number from day 1 to day " + maxSol;
 
@@ -34,43 +26,22 @@ document.addEventListener('DOMContentLoaded', () => {
 			selectSolarDay.value = "";
 			selectSolarDay.disabled = false;
 
-
-
-			// processData(data.photos);
-
 		}).fail(function(jqXHR, textStatus, errorThrown){
 			console.log(textStatus, jqXHR.status, jqXHR.statusText);
 		});
 	}
 
 
-
-
-
-
-
-
-
 	let selectedRover;
 	let selectedSol;
 	let selectedCamera;
-
-
-
-
-
-
-
 	const selectRover = document.getElementById("inputRover");
 	const selectSolarDay = document.getElementById("inputSol");
 	let selectCamera = document.getElementById("inputCamera");
 	let solDescript = document.getElementById("solDescript");
 
 	selectRover.addEventListener('change', (e) => {
-
 		if (!e.target.value) {
-			console.log('not');
-			console.log(e.target.value);
 			selectSolarDay.value = '';
 			selectSolarDay.disabled = true;
 			selectCamera.value = '';
@@ -84,32 +55,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	selectSolarDay.addEventListener('keyup', (e) => {
 		if(/^[0-9]+$/.test(e.target.value)){
-			console.log('keyup', e.target.value);
-
 			debounceInput(e.target.value);
-			// addCameras(e.target.value);
-
-
 		}
 	});
 
 	/* disable */
 	selectSolarDay.addEventListener('keydown', (e) => {
 		selectCamera.disabled = true;
-		console.log('disable');
-
 	});
 
 
 	selectCamera.addEventListener('change', (e) => {
-		console.log('camera input change');
 		selectedCamera = e.target.value;
-		console.log(selectedCamera);
-
 	});
 
-
-	// selectSolarDay.addEventListener('keyup', (e) => { });
 
 	function solarDayError(found) {
 		solDescript.innerText = "No photos for this day. Next day with photos is " + found;
@@ -129,11 +88,9 @@ document.addEventListener('DOMContentLoaded', () => {
 			let index;
 			let found = solsWithPics.find(function(item, i) {
 				index = i;
-				console.log(item, solNumber);
 				if (item == solNumber) return true;
 				return item > solNumber;
 			});
-			console.log('found', found);
 
 			if (solNumber != found) {
 				solarDayError(found);
@@ -143,24 +100,17 @@ document.addEventListener('DOMContentLoaded', () => {
 				solDescript.innerText = "Please enter a number from day 1 to day " + maxSol;
 				selectedSol = sol;
 
-				// (function(){
-					cameraTemplate = `<option value=""></option>`;
-				console.log(index);
+				cameraTemplate = `<option value=""></option>`;
 
-					manifest.photos[index].cameras.forEach(camera => {
-						console.log("camera", camera);
-
+				manifest.photos[index].cameras.forEach(camera => {
 					cameraTemplate += `<option value="${camera}">${camera}</option>`;
 				});
 
 				selectCamera.disabled = false;
 				selectCamera.innerHTML = cameraTemplate;
-				// }());
 			}
-
 		}, 1000);
 	}
-
 
 
 	// set submit function here
@@ -172,29 +122,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	function getImages(roverNameQsValue, solDayQSValue, cameraQsValue) {
-		console.log(roverNameQsValue, solDayQSValue, cameraQsValue);
-
 		// let requestUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverNameQsValue}/photos?sol=${solDayQSValue}&page=1&api_key=${marsRoverApiKey}`;
 		let requestUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverNameQsValue}/photos?sol=${solDayQSValue}&page=1&camera=${cameraQsValue}&api_key=c9OyMPxnO1YTXXEecRMoGxlfNRRMHT412OFVjMyA`;
-
 
 		$.ajax({
 			method: "GET",
@@ -214,19 +144,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-	// getImages('Curiosity', 22, 'CHEMCAM')
-	// getImages('curiosity', 22, 'navcam')
-	// getImages('curiosity', 33, 'FHAZ')
-	// getImages('curiosity', 33, 'RHAZ')
-
-
-
-	//
-		// let headersList = {
-		// 	"Accept": "*/*",
-		// 	"User-Agent": "Thunder Client (https://www.thunderclient.com)"
-		// }
-	//
 	/*
 		fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&page=1&api_key=${marsRoverApiKey}", {
 			method: "GET",
@@ -243,20 +160,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	let myTemplate;
 
 	function processData(photos) {
-		console.log('photos', photos);
-
 		myTemplate = '';
-
-		// let rover, camera, imgSrc;
 
 		photos.forEach(row => {
 			makeTemplate(row.rover.name, row.camera.name, row.img_src, row.earth_date);
-			console.log(row.camera.name);
-
 		});
 
 		function makeTemplate(rover, camera, imgSrc, earthDate) {
-			let tempTemplate = `<div class="col-12 col-md-6 col-lg-4 col-xl-3"><img src="${imgSrc}" alt=""> <div class="text-end">Date: ${earthDate} </div></div>`;
+			let tempTemplate = `<div class="col"><img src="${imgSrc}" alt=""> <div class="text-end">Date: ${earthDate} </div></div>`;
 
 			myTemplate += tempTemplate;
 
@@ -267,18 +178,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			document.getElementById("cameraName").innerHTML = longName;
 			document.getElementById("test").innerHTML = myTemplate;
 		}
-
-
-
 	}
-
-
-
-
-
-
-
-
 
 
 
